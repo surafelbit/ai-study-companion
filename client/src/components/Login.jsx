d773fe6e8edc25b586811b74cb186d2a7094820b
Your Real Name <surafelmuhabaw17@gmail.com>
2025-11-09 14:11:24 +0300

    login page reform

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,56 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import { useAuth } from "../hooks/AuthProvider";
 import { useNavigate } from "react-router-dom";
-// export default function Login() {
-//   const { login } = useAuth();
-//   const navigator = useNavigate();
-//   const [email, setEmail] = useState();
-//   const [password, setPassword] = useState();
-//   const [error, setError] = useState(false);
-//   async function submitHandler(e) {
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post(
-//         "http://localhost:5000/api/auth/login",
-//         {
-//           email,
-//           password,
-//         }
-//       );
-//       console.log(response.data);
-//       // localStorage.setItem("user", response.data);
-//       login(response.data);
-//       navigator("/fanum");
-//       setError(false);
 
-//       console.log(response);
-//     } catch (error) {
-//       console.log(error);
-//       setError(true);
-//     }
-//   }
-//   return (
-//     <div>
-//       <form onSubmit={submitHandler}>
-//         <label>Email</label>
-//         <input
-//           value={email}
-//           onChange={(e) => setEmail(e.target.value)}
-//           type="email"
-//           required
-//         ></input>
-//         <label>Password</label>
-//         <input
-//           value={password}
-//           onChange={(e) => setPassword(e.target.value)}
-//           type="password"
-//           required
-//         ></input>
-//         <button type="submit">Login</button>
-//       </form>
-//     </div>
-//   );
-// }
 export default function Login() {
   const { login } = useAuth();
   const navigator = useNavigate();
