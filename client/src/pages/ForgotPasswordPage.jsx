2b72032ee6335eec27752ec5e0c4e3268edbb580
Your Real Name <surafelmuhabaw17@gmail.com>
2025-11-22 08:43:37 +0300

    forgot password page update

--- client/src/pages/ForgotPasswordPage.jsx
+++ client/src/pages/ForgotPasswordPage.jsx
@@ -2,37 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useState } from "react";
 import { toast } from "react-toastify";
-// export default function ForgotPasswordPage() {
-//   const [email, setEmail] = useState();
-//   async function submitter(e) {
-//     e.preventDefault();
-//     try {
-//       const response = await axios.post(
-//         "http://localhost:5000/api/auth/forgotpassword",
-//         { email }
-//       );
-//       console.log(response);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-//   return (
-//     <div>
-//       <h1>forgot password</h1>
-//       <form onSubmit={submitter}>
-//         <h1>input email</h1>
-//         <label>email</label>
-//         <input
-//           type="email"
-//           onChange={(e) => {
-//             setEmail(e.target.value);
-//           }}
-//         ></input>
-//         <button type="submit">submit</button>
-//       </form>
-//     </div>
-//   );
-// }
+
 export default function ForgotPasswordPage() {
   const [email, setEmail] = useState();
 
