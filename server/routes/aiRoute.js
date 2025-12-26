1aaec3330d27e38211d386761f9572f2c8d4913b
Your Real Name <surafelmuhabaw17@gmail.com>
2025-12-26 18:25:30 +0300

    update

--- server/routes/aiRoute.js
+++ server/routes/aiRoute.js
@@ -4,4 +4,4 @@ const aiResponseController = require("../controllers/aiResponseController");
 router.get("/history");
 router.post("/ask", aiResponseController.ask);
 module.exports = router;
-//
+//ok
