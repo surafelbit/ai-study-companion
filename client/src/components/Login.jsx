8a98fdb9060638affb706856d32d6fa942aa6d5b
Your Real Name <surafelmuhabaw17@gmail.com>
2026-01-25 23:18:24 +0300

    update

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
   const [error, setError] = useState(false);
-  //
+
   async function submitHandler(e) {
     e.preventDefault();
     try {
