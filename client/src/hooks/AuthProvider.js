13152ca14a8d3bf12dd69e5bf914de5877a85425
Surafel Muhabaw <122958364+surafelbit@users.noreply.github.com>
2026-01-27 21:48:27 +0300

    Update AuthProvider.js

--- client/src/hooks/AuthProvider.js
+++ client/src/hooks/AuthProvider.js
@@ -1,6 +1,6 @@
 import { useContext, createContext, useEffect, useState } from "react";
 const AuthContext = createContext();
-
+//
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
